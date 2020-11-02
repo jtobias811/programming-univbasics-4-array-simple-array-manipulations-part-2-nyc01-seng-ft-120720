efdce111b20727bb4f7098d9f1e70b32349ca57e
Jacob Tobias <jtobias811@gmail.com>
2020-11-02 15:29:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert(array, language)
+def using_insert(array, "language")
   new_array = array.insert("Python")
 end
